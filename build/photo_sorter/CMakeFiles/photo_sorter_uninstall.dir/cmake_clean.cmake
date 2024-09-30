c239346f22843c904f8a59cc68aa68e3a0c9b5a6
luka_x240 <404527001@qq.com>
2024-09-30 15:46:35 +0800

    add photo_sorter my_node

---
+++ build/photo_sorter/CMakeFiles/photo_sorter_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/photo_sorter_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/photo_sorter_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/photo_sorter/CMakeFiles/photo_sorter_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for photo_sorter_uninstall.
+# This may be replaced when dependencies are built.
