81df2c574a48e5992ecce9806e28ac8eb671d23d
luka_x240 <404527001@qq.com>
2024-09-30 15:56:29 +0800

    update .gitignore

--- build/photo_sorter/CMakeFiles/photo_sorter_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/photo_sorter_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/photo_sorter_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
